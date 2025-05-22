ab68c66b71415158c96460a4ab06a06379e96eda
Ariel R <resendiz.ariel6@gmail.com>
2025-05-21 20:23:59 -0700

    HomePage updated and added Products button using BootStrap

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <h1 className="my-5">Welcome</h1>
+      <Link to="/products">
+        <Button
+          variant="primary"
+          className="text-warning border border-3 border-secondary rounded"
+        >
+          View all products
+        </Button>
+      </Link>
+    </>
+  );
 };
 
 export default HomePage;
